0c975921be05bd4c68a3ed8db6dd70f68936d4f2
DE_NARDI Lenny <o22106780@info03-19.iut45.univ-orleans.fr>
2023-04-03 17:41:36 +0200

    ajout du style pour l'affichage de tous les articles

--- lib/article_liste.dart
+++ lib/article_liste.dart
@@ -26,15 +26,88 @@ class _ArticleListState extends State<ArticleList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return GridView.builder(
+      padding: const EdgeInsets.all(8.0),
       itemCount: articles.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.7,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(articles[index]['title']),
-          subtitle: Text(articles[index]['description']),
+        return GestureDetector(
           onTap: () {
-            // TODO: Naviguer vers la fiche de l'article sélectionné
+            Navigator.pushNamed(
+              context,
+              '/detail',
+              arguments: articles[index],
+            );
           },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[300]!,
+                  blurRadius: 3.0,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(articles[index]['images'][0]),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        articles[index]['title'],
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 4.0),
+                      Text(
+                        articles[index]['description'],
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(fontSize: 14.0),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        "${articles[index]['price']} €",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange[900],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
